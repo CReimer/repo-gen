c4928e21dcf40ede283d5e1ea810d11130a3040a
Christopher Reimer <mail+gitproject@c-reimer.de>
2016-02-23 17:07:24 +0100

    Fix some errors detect by PyCharm's inspector

--- docker-armv7/mkimage.sh
+++ docker-armv7/mkimage.sh
@@ -16,6 +16,6 @@ pacstrap -C ${PACMAN_CONF} -c -d -G ${ROOTFS} base-devel haveged
 #fakechroot fakeroot pacman --root=${ROOTFS} --config=${TMP_CONF} -Sy base-devel haveged
 
 
-tar --numeric-owner --xattrs --acls -C ${ROOTFS} -c . -f $DOCKER_IMAGE_NAME-$(date "+%Y%m%d").tar.gz
-ln -sf $DOCKER_IMAGE_NAME-$(date "+%Y%m%d").tar.gz $DOCKER_IMAGE_NAME-latest.tar.gz
+tar --numeric-owner --xattrs --acls -C ${ROOTFS} -c . -f ${DOCKER_IMAGE_NAME}-$(date "+%Y%m%d").tar.gz
+ln -sf ${DOCKER_IMAGE_NAME}-$(date "+%Y%m%d").tar.gz ${DOCKER_IMAGE_NAME}-latest.tar.gz
 rm -rf ${ROOTFS}
