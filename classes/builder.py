8e059fb9e80487f700f22fa9f5784acbf03b9eca
Christopher Reimer <mail+gitproject@c-reimer.de>
2016-03-06 19:20:34 +0100

    Fix Error in Builder

--- classes/builder.py
+++ classes/builder.py
@@ -38,7 +38,7 @@ class Builder:
         fnull.close()
 
     def build(self, pkgbase):
-            MessagePrinter.print_msg('Building ' + pkgbase)
+            MessagePrinter.print_msg('Building ' + pkgbase.directory)
             fnull = open(os.devnull, 'w')
             subprocess.call(['docker', 'rm', self.tmp_name], stdout=fnull, stderr=fnull)
             fnull.close()
