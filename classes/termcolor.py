49911147bbe4663423ed24bd04dc977d69fa9835
Christopher Reimer <mail+gitproject@c-reimer.de>
2017-02-03 12:34:00 +0100

    Move TermColor into messageprinter.py

--- classes/termcolor.py
+++
@@ -1,7 +0,0 @@
-class TermColor:
-    BOLD = '\033[1m'
-    BOLD_RED = '\033[1;31m'
-    BOLD_GREEN = '\033[1;32m'
-    BOLD_YELLOW = '\033[1;33m'
-    BOLD_BLUE = '\033[1;34m'
-    RESET = '\033[0m'
